79fd5c7cbe7e6c328222ffc55bda9c250034b093
Kusuma Hendra <k.kusumahendra@gmail.com>
2021-07-12 17:37:41 +0800

    #21 feat: partners BE and integration

--- resources/js/component-sets/partner.js
+++ resources/js/component-sets/partner.js
@@ -6,14 +6,21 @@ require("slick-carousel/slick/slick");
 require("slick-carousel/slick/slick.css");
 
 $(function () {
-    $('.slick-partners').slick({
-        // dots          : true,
-        arrows         : true,
-        slidesToShow: 5,
-        slidesToScroll: 1,
-        // infinite      : true,
-        // adaptiveHeight: true,
-        autoplay      : false,
-        autoplaySpeed : 3000
-    })
+    $('.slick-partners').each(function () {
+        let slideNumber = 5;
+        if ($(this).attr('data-slide-number')) {
+            slideNumber = $(this).attr('data-slide-number');
+        }
+
+        $(this).slick({
+            // dots          : true,
+            arrows        : true,
+            slidesToShow  : slideNumber,
+            slidesToScroll: 1,
+            // infinite      : true,
+            // adaptiveHeight: true,
+            autoplay     : false,
+            autoplaySpeed: 3000
+        })
+    });
 });
