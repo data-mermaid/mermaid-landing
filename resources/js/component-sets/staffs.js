05f503ac5176749c209470bdb43867d8de185d88
Kusuma Hendra <k.kusumahendra@gmail.com>
2021-07-16 15:09:01 +0800

    #25 feat: staff show detail sync

--- resources/js/component-sets/staffs.js
+++ resources/js/component-sets/staffs.js
@@ -1,6 +1,7 @@
 $(function(){
     $('.team .open-details').click(function(e){
         e.preventDefault();
+        $('.staff, .team').removeClass('display-details');
         $(this).parents('.staff, .team').addClass('display-details');
     })
     $('.team .team-details .btn-close').click(function(){
