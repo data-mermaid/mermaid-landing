8bedc1bba00f86f0fbd2302d04e516ae456ed750
Kusuma Hendra <k.kusumahendra@gmail.com>
2021-07-23 11:47:18 +0800

    #28 feat: sticky nav update

--- resources/js/partials/header.js
+++ resources/js/partials/header.js
@@ -33,4 +33,42 @@ $(function () {
             oldScroll = $(window).scrollTop();
         }
     }, 250);
+
+    $('#main-header .navbar-toggler, #sticky-header .navbar-toggler').click(function(){
+        $('#mobile-header').toggleClass('open')
+    })
+
+    $('#mobile-header .navbar-toggler').click(function(){
+        $('#mobile-header').toggleClass('open')
+    })
+
+    setTimeout(function(){
+        $navbar = $('#main-header');
+        if(window.pageYOffset === 0){
+            $navbar.removeClass("reveal");
+            $navbar.removeClass("vanish");
+            $navbar.addClass("top");
+        } else {
+            $navbar.addClass("reveal");
+            $navbar.removeClass("vanish");
+            $navbar.removeClass("top");
+        }
+    }, 300);
+    setInterval(function(){
+        var prevScrollpos = window.pageYOffset;
+        window.onscroll = function () {
+            var currentScrollPos = window.pageYOffset;
+            $navbar = $('#main-header');
+
+            if (prevScrollpos > currentScrollPos) {
+                if (currentScrollPos === 0) {
+                    $navbar.addClass("top").removeClass("reveal").removeClass("vanish");
+                } else {
+                    $navbar.addClass("reveal").removeClass("vanish").removeClass("top");
+                }
+            } else {
+                $navbar.addClass("vanish").removeClass("reveal").removeClass("top");
+            }
+        };
+    },300);
 })
