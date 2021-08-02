8d05505fe5bb76a3686197960155657f1d913200
Kusuma Hendra <k.kusumahendra@gmail.com>
2021-08-02 13:59:36 +0800

    #31 feat: Mer-Def-026

--- resources/js/component-sets/slideshow.js
+++ resources/js/component-sets/slideshow.js
@@ -9,7 +9,7 @@ $(function () {
     $('.slick-slideshow').slick({
         dots          : true,
         infinite      : true,
-        adaptiveHeight: true,
+        adaptiveHeight: false,
         autoplay      : true,
         autoplaySpeed : 3000
     })
