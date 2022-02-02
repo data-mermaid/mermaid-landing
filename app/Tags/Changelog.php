0981cb9e54283a844e400b991e0f5cc9a4b9b4b6
Kusuma Hendra <hendra@catalyzecommunications.com>
2022-02-02 10:32:19 +0800

    #48 fix: changelog 1 hour mark update from source

--- app/Tags/Changelog.php
+++ app/Tags/Changelog.php
@@ -20,7 +20,9 @@ class Changelog extends Tags
         }
         $lastModified = Storage::disk('public')->lastModified(config('app.changelog_path'));
 
-        if (Carbon::parse($lastModified)->isLastHour()) fetchChangelog();
+        if (Carbon::now()->diffInHours(Carbon::parse($lastModified)) > 1 ){
+            fetchChangelog();
+        }
 
         $path = Storage::disk('public')->path(config('app.changelog_path'));
         $json = json_decode(file_get_contents($path), true);
